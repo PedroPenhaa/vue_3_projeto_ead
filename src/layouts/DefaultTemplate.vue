aca3e2084219c7658e1363d38317854ffe18bd48
pedropenhavn <pedro.afonso@vilanova.com.br>
2023-12-27 14:21:04 -0300

    layouts

---
+++ src/layouts/DefaultTemplate.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <header-component />
+
+    <section>
+      <router-view />
+    </section>
+
+    <footer-component />
+  </div>
+</template>
+
+
+
+<script>
+import FooterComponentVue from "./Components/FooterComponent.vue";
+
+import HeaderComponent from "./Components/HeaderComponent.vue";
+import FooterComponent from "./Components/FooterComponent.vue";
+
+export default {
+  name: "DefaultTemplate",
+  components: {
+    HeaderComponent,
+    FooterComponent,
+  },
+};
+</script>

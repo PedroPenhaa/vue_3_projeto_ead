d26900b681cd754ef5633516628499cf77b8548b
PedroPenhaa <pedro.eng98@gmail.com>
2024-01-02 12:55:15 -0300

    sad

--- src/layouts/Components/HeaderComponent.vue
+++ src/layouts/Components/HeaderComponent.vue
@@ -3,7 +3,10 @@
     <nav>
       <span class="toggleMenu far fa-bars"></span>
       <span class="logo">
-        <img :src="require('@/assets/images/logo.svg')" alt="Especializa TI" />
+
+        <router-link :to="{ name: 'campus.home' }">
+             <img :src="require('@/assets/images/logo.svg')" alt="Especializa TI" />
+        </router-link>
       </span>
     </nav>
   </header>
--- src/views/auth/Auth.vue
+++ src/views/auth/Auth.vue
@@ -49,7 +49,8 @@
                 <input type="password" name="password" placeholder="Senha" required>
                 <i class="far fa-eye buttom"></i>
               </div>
-              <button class="btn primary" type="submit">Login</button>
+              <button class="btn primary" type="submit" @click.prevent="login">Login</button>
+
             </form>
             <span>
                             <p class="fontSmall">Esqueceu sua senha?
@@ -72,8 +73,18 @@
 
 
 <script>
+  import router from "@/router";
+
+
   export default {
     // eslint-disable-next-line vue/multi-word-component-names
     name: "Auth",
+    setup() {
+        const login = () => router.push({name: 'campus.home'})
+
+        return{
+          login
+        }
+    },
   };
 </script>
